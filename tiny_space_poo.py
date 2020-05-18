ccc1c0d408a469f4852f79f1dbde25fe5478ad2f
Michael White <github@mypalmike.com>
2020-05-18 11:44:59 -0700

    Add asciigalaxy check.

--- tiny_space_poo.py
+++ tiny_space_poo.py
@@ -19,10 +19,11 @@ UNICODE_POO = '\U0001F4A9'
 ID_MYPALMIKE = '233950108'
 ID_TINY_ASTRO_NAUT = '2758649640'
 ID_DIGITAL_HENGE = '3062148770'
-
+ID_ASCIIGALAXY = '980223140574806017'
 
 def space_indices(text):
-  return [i for i, x in enumerate(text) if x == ' ']
+  # Spaces or the wide dot used by asciigalaxy
+  return [i for i, x in enumerate(text) if x in (' ', '．')]
 
 
 def mangle_status_fill(text, sender):
@@ -85,7 +86,7 @@ class TinySpacePooListener(tweepy.StreamListener):
 
     mangled_status = None
     if 'tiny_space_poo' not in status.text:  # Avoid bot loops by ignoring tweets mentioning me.
-      if status.author.screen_name.lower() in ('tiny_astro_naut'): # , 'mypalmike'):
+      if status.author.screen_name.lower() in ('tiny_astro_naut', 'asciigalaxy'): # , 'mypalmike'):
         logging.info('Matched fill-space user')
         mangled_status = mangle_status_fill(status.text, status.author.screen_name)
       elif status.author.screen_name.lower() in ('digital_henge'):
@@ -140,7 +141,7 @@ def main(argv = sys.argv):
 
   while True:
     try:
-      stream.filter(follow=[ID_MYPALMIKE, ID_TINY_ASTRO_NAUT, ID_DIGITAL_HENGE])
+      stream.filter(follow=[ID_MYPALMIKE, ID_TINY_ASTRO_NAUT, ID_DIGITAL_HENGE, ID_ASCIIGALAXY])
     except Exception as exc:
       if exc.args and 'timed out' in exc.args:
         pass
@@ -150,3 +151,4 @@ def main(argv = sys.argv):
 
 if __name__ == '__main__':
   main()
+  
